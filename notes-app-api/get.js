d82ec83252e914ed5ffc3bc5ad1076a5b9174839
Jon Richards <jon@jonrichards.net>
2018-06-07 15:36:51 -0700

    Add list api (#4)

--- notes-app-api/get.js
+++ notes-app-api/get.js
@@ -22,7 +22,6 @@ export async function main(event, context, callback) {
             callback(null, failure({ status: false, error: "Item not found." }));
         }
     } catch (e) {
-        console.log(e);
         callback(null, failure({ status: false }));
     }
 }
